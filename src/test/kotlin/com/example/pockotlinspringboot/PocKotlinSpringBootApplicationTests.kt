dad8f4ff0f1b9dd506097800e1e3c80209ba7798
Gastón Vidal <gnvidal25@gmail.com>
2024-06-07 15:56:14 -0300

    Initial commit

---
+++ src/test/kotlin/com/example/pockotlinspringboot/PocKotlinSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pockotlinspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocKotlinSpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
