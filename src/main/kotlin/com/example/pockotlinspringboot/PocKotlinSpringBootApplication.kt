8aada8f35340304cd547a1b28eaa188999f0d47c
Gastón Vidal <gnvidal25@gmail.com>
2024-06-14 19:46:20 -0300

    Database setup

--- src/main/kotlin/com/example/pockotlinspringboot/PocKotlinSpringBootApplication.kt
+++ src/main/kotlin/com/example/pockotlinspringboot/PocKotlinSpringBootApplication.kt
@@ -2,8 +2,13 @@ package com.example.pockotlinspringboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @SpringBootApplication
 class PocKotlinSpringBootApplication
@@ -13,16 +18,31 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController {
-    // listOf -> readOnly
+class MessageController(val service: MessageService) {
     @GetMapping("/")
-    fun index() = listOf<Message>(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-    )
+    fun index() = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message){
+        service.save(message)
+    }
 }
 
 // val -> immutable
 // var -> mutable
 data class Message(val id: String?, val text: String)
+
+@Service
+class MessageService(private val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message) {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "insert into messages values (?,?)",
+            id, message.text,
+        )
+    }
+}
